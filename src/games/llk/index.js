f8bd7df3c84dcbfbf3cecc04709c0555b4c587a1
jser.li <jser.li@circcus.com>
2018-11-21 23:31:18 +0800

    完成连连看游戏开发

--- src/games/llk/index.js
+++ src/games/llk/index.js
@@ -21,11 +21,11 @@ class Game {
     if ((rows * cols) / (imgOptions.rows * imgOptions.cols)  % 2) {
       throw new Error('行和列的积除以雪碧图的行和列的积必须是2的倍数.')
     }
-    this.rows = rows
-    this.cols = cols
+    this.rows = rows + 2
+    this.cols = cols + 2
     this.imgOptions = imgOptions
     this.blockSpace = this.pixRatio * 3
-    this.selBlock = this.curBlock = null
+    this.selBlock = null
     this.joinPoints = []
     this.updateSize()
     utils.imageLoad(imgOptions.src).then(img => {
@@ -39,8 +39,8 @@ class Game {
     let { pixRatio, rows, cols, blockSpace } = this
     let width = this.canvas.offsetWidth
     this.width = this.canvas.width = width * pixRatio
-    this.blockSize = (this.width - (cols + 1) * blockSpace) / (cols + 1)
-    this.height = this.canvas.height = (this.blockSize + blockSpace) * (rows + 1)
+    this.blockSize = (this.width - (cols - 1) * blockSpace) / (cols - 1)
+    this.height = this.canvas.height = (this.blockSize + blockSpace) * (rows - 1)
   }
 
   onClick(event) {
@@ -68,52 +68,23 @@ class Game {
   }
 
   getCenter(block) {
-    return [block.x + this.blockSize / 2, block.y + this.blockSize / 2]
+    return [block.x + block.w / 2, block.y + block.h / 2]
   }
 
   findWay(b1, b2) {
-    return this.lineDirect(b1, b2) || this.edgeDirect(b1, b2) || this.oneCorner(b1, b2) || this.twoCorner(b1, b2)
-  }
-
-  edgeDirect(b1, b2) {
-    let { blockSize, width, height } = this
-    if (b1.col === b2.col) {
-      if (b1.col === 0) {
-        return [
-          { x: blockSize / 4, y: b1.y + blockSize / 2 },
-          { x: blockSize / 4, y: b2.y + blockSize / 2 }
-        ]
-      } else if (b1.col === this.cols - 1) {
-        return [
-          { x: width - blockSize / 4, y: b1.y + blockSize / 2 },
-          { x: width - blockSize / 4, y: b2.y + blockSize / 2 }
-        ]
-      }
-    } else if (b1.row === b2.row) {
-      if (b1.row === 0) {
-        return [
-          { x: b1.x + blockSize / 2, y:  blockSize / 4 },
-          { x: b2.x + blockSize / 2, y: blockSize / 4 }
-        ]
-      } else if (b1.row === this.rows - 1) {
-        return [
-          { x: b1.x + blockSize / 2, y: height - blockSize / 4 },
-          { x: b2.x + blockSize / 2, y: height - blockSize / 4 }
-        ]
-      }
-    }
+    return this.lineDirect(b1, b2) || this.oneCorner(b1, b2) || this.twoCorner(b1, b2)
   }
 
   lineDirect(b1, b2) {
     let arr = []
     if (b1.row !== b2.row && b1.col !== b2.col) return false
     if (b1.col === b2.col) {
-      arr = this.blocks.filter(_ => _.col === b1.col)
+      arr = this.blocks.filter(_ => _ && _.col === b1.col)
       arr = b1.row > b2.row ?
         arr.filter(_ => _.row < b1.row && _.row > b2.row) :
         arr.filter(_ => _.row > b1.row && _.row < b2.row)
     } else if (b1.row === b2.row) {
-      arr = this.blocks.filter(_ => _.row === b1.row)
+      arr = this.blocks.filter(_ => _ && _.row === b1.row)
       arr = b1.col > b2.col ?
         arr.filter(_ => _.col < b1.col && _.col > b2.col) :
         arr.filter(_ => _.col > b1.col && _.col < b2.col)
@@ -122,21 +93,21 @@ class Game {
   }
 
   oneCorner(b1, b2) {
-    let c = this.blocks.find(_ => _.row === b1.row && _.col === b2.col)
+    let c = this.blocks.find(_ => _ && _.row === b1.row && _.col === b2.col)
     if (!c.num && this.lineDirect(b1, c) && this.lineDirect(c, b2)) return [c]
-    c = this.blocks.find(_ => _.row === b2.row && _.col === b1.col)
+    c = this.blocks.find(_ => _ && _.row === b2.row && _.col === b1.col)
     if (!c.num && this.lineDirect(b1, c) && this.lineDirect(c, b2)) return [c]
     return false
   }
 
   twoCorner(b1, b2) {
-    let sameRows = this.blocks.filter(_ => _ !== b1 && _.row === b1.row)
-    let sameCols = this.blocks.filter(_ => _ !== b1 && _.col === b1.col)
+    let sameRows = this.blocks.filter(_ => _ && _ !== b1 && _.row === b1.row)
+    let sameCols = this.blocks.filter(_ => _ && _ !== b1 && _.col === b1.col)
     let upBlocks = sameCols.filter(_ => _.row < b1.row)
     let rightBlocks = sameRows.filter(_ => _.col > b1.col)
     let downBlocks = sameCols.filter(_ => _.row > b1.row)
     let leftBlocks = sameRows.filter(_ => _.col < b1.col)
-    for (let i = upBlocks.length - 1; i > 0; i--) {
+    for (let i = upBlocks.length - 1; i >= 0; i--) {
       let _ = upBlocks[i]
       if (_.num) break
       let arr = this.oneCorner(_, b2)
@@ -154,7 +125,7 @@ class Game {
       let arr = this.oneCorner(_, b2)
       if (arr) return [_, ...arr]
     }
-    for (let i = leftBlocks.length - 1; i > 0; i--) {
+    for (let i = leftBlocks.length - 1; i >= 0; i--) {
       let _ = leftBlocks[i]
       if (_.num) break
       let arr = this.oneCorner(_, b2)
@@ -164,7 +135,7 @@ class Game {
 
   doneCheck() {
     let { onDone = () => {} } = this.callbacks
-    return this.blocks.every(_ => !_.num) && setTimeout(onDone, 100)
+    return this.blocks.every(_ => !(_ && _.num)) && setTimeout(onDone, 100)
   }
 
   createSprites() {
@@ -175,7 +146,7 @@ class Game {
         sprites.push({ dx: dw * col, dy: dw * row, dw })
       }
     }
-    let len = this.rows * this.cols / sprites.length
+    let len = (this.rows - 2) * (this.cols - 2) / sprites.length
     for (let i = 0; i < len; i++) rtnSprites.push(...sprites)
     rtnSprites.sort(() => Math.random() - .5)
     return rtnSprites
@@ -183,16 +154,29 @@ class Game {
 
   genBlocks() {
     let blocks = [], sprites = this.createSprites(), count = 0
-    let { rows, cols, blockSize, blockSpace } = this
+    let { rows, cols, blockSize: w, blockSpace: space } = this
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
-        let { dx, dy, dw } = sprites[count]
-        blocks.push({
-          row, col, dx, dy, dw, num: 1,
-          x: (blockSize + blockSpace) * col + blockSpace + blockSize / 2,
-          y: (blockSize + blockSpace) * row + blockSpace + blockSize / 2
-        })
-        count++
+        let x = (w + space) * (col - 1) + space + w / 2
+        let y = (w + space) * (row - 1) + space + w / 2
+        if ((col === 0 && row === 0) || 
+          (col === cols - 1 && row === 0) || 
+          (col === 0 && row === rows - 1) || 
+          (col === cols - 1 && row === rows - 1)) {
+          blocks.push(null)
+        } else if (col === 0) {
+          blocks.push({ row, col, num: 0, x: 0, y, w: w / 2, h: w })
+        } else if (col === cols - 1) {
+          blocks.push({ row, col, num: 0, x, y, w: w / 2, h: w })
+        } else if (row === 0) {
+          blocks.push({ row, col, num: 0, x, y: 0, w, h: w / 2 })
+        } else if (row === rows - 1) {
+          blocks.push({ row, col, num: 0, x, y, w, h: w / 2 })
+        } else {
+          let { dx, dy, dw } = sprites[count]
+          blocks.push({ row, col, dx, dy, dw, num: 1, x, y, w, h: w })
+          count++
+        }
       }
     }
     return blocks
@@ -200,19 +184,24 @@ class Game {
 
   rinse() {
     this.selBlock = null
-    let coords = this.blocks.map(_ => ({ dx: _.dx, dy: _.dy })).sort(() => Math.random() - .5)
-    this.blocks.forEach((_, i) => {
-      let coord = coords[i]
-      _.dx = coord.dx
-      _.dy = coord.dy
-    })
+    let coords = [], count = 0
+    this.blocks.forEach(_ => _ && _.dw && _.num && coords.push({ dx: _.dx, dy: _.dy }))
+    coords.sort(() => Math.random() - .5)
+    for (let i = 0; i < this.blocks.length; i++) {
+      let _ = this.blocks[i]
+      if (_ && _.dw && _.num) {
+        _.dx = coords[count].dx
+        _.dy = coords[count].dy
+        count++
+      }
+    }
     this.drawUI()
   }
 
   drawUI() {
     this.context.clearRect(0, 0, this.width, this.height)
     this.blocks.forEach(_ => {
-      _.num && this.context.drawImage(this.img, _.dx, _.dy, _.dw, _.dw, _.x, _.y, this.blockSize, this.blockSize)
+      _ && _.num && this.context.drawImage(this.img, _.dx, _.dy, _.dw, _.dw, _.x, _.y, _.w, _.h)
     })
     if (this.selBlock) this.drawArc(this.selBlock)
   }
@@ -221,7 +210,7 @@ class Game {
     this.context.save()
     this.context.fillStyle = `rgba(0, 0, 0, ${alpha})`
     this.context.beginPath()
-    this.context.arc(...this.getCenter(block), this.blockSize / 2, 0, Math.PI * 2)
+    this.context.arc(...this.getCenter(block), block.w / 2, 0, Math.PI * 2)
     this.context.closePath()
     this.context.fill()
     this.context.restore()
@@ -235,25 +224,19 @@ class Game {
     context.lineWidth = pixRatio
     context.beginPath()
     context.moveTo(...this.getCenter(b1))
-    blocks.forEach(_ => {
-      if (_.row !== undefined) {
-        this.context.lineTo(...this.getCenter(_))
-      } else {
-        this.context.lineTo(_.x, _.y)
-      }
-    })
+    blocks.forEach(_ => this.context.lineTo(...this.getCenter(_)))
     context.stroke()
     context.restore()
   }
 
   getCurBlock(event) {
-    let { blockSize } = this
     let ex = (event.offsetX || event.pageX) * this.pixRatio
     let ey = (event.offsetY || event.pageY) * this.pixRatio
     return this.blocks.find(_ => {
-      let x = _.x + blockSize / 2
-      let y = _.y + blockSize / 2
-      return _.num && (ex - x) * (ex - x)+(ey - y) * (ey - y) < blockSize * blockSize / 4
+      if (!_) return
+      let x = _.x + _.w / 2
+      let y = _.y + _.h / 2
+      return _.num && (ex - x) * (ex - x)+(ey - y) * (ey - y) < _.w * _.h / 4
     })
   }
 }
